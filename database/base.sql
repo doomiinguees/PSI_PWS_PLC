16f1d63a30c649f35fed1d0e5c14a68024bbba14
doomiinguees <114529154+doomiinguees@users.noreply.github.com>
2023-06-29 02:33:31 +0100

    Base de dados exportada

--- database/base.sql
+++ database/base.sql
@@ -18,6 +18,7 @@
 --
 -- Table structure for table `empresas`
 --
+
 DROP TABLE IF EXISTS `empresas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
